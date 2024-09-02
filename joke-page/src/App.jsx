a3db443bd05b8a508f8c1b7e317d0cb798fd3471
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-09-02 09:20:22 +0200

    We out here and started

---
+++ joke-page/src/App.jsx
@@ -0,0 +1,15 @@
+import Joke from "./components/Joke/Joke"
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <Joke/>
+    </>
+  )
+}
+
+export default App
---
+++ joke-page/src/components/Joke/Joke.jsx
@@ -0,0 +1,15 @@
+import style from '../Joke/Joke.module.scss';
+
+
+function Joke() {
+ 
+
+    return (
+      <>  
+      <h1>Joke app with API</h1>
+      </>
+    )
+  }
+  
+  export default Joke
+
